1e8fedb1b3ba89504b1db5659f207feda20de63a
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-18 15:59:33 -0800

    Add script to seed database

---
+++ database/seed-data/seeder.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const ProductFeatures = require('../database.js');
+const { fakeDataGenerator } = require('./fakeDataGenerator.js');
+
+(async function seedDatabase() {
+  // load fakeDataGenerator with (recordsToCreate, startingRecordID)
+  let fakeData = await fakeDataGenerator(100, 1000);
+
+  console.log(`Seeding database with ${fakeData.length} record(s).`);
+
+  ProductFeatures.insertMany(fakeData)
+    .then(() => console.log('Database seeding complete.'))
+    .catch((err) => console.log('Error seeding database', err))
+    .finally(() => mongoose.connection.close());
+})();
