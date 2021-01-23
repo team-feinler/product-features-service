7213d8dafdef008a7e7cb8ba5d29a5cbae77d485
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-22 21:06:16 -0800

    [WIP] Tests

--- test/db-test.js
+++ test/db-test.js
@@ -6,30 +6,35 @@ const db = mongoose.connection;
 const ProductFeatures = require('../database/database.js');
 const { fakeDataGenerator } = require('../database/seed-data/fakeDataGenerator.js');
 
-describe('Database seeding', function() {
-  describe('Fake Data Generator', function() {
-    it('should generate 100 records when 100 records are requested', function() {
+describe('Database seeding', () => {
+  describe('Fake Data Generator', () => {
+    it('should generate 100 records when 100 records are requested', () => {
       let fakeTestData = fakeDataGenerator(100, 0);
       assert.equal(fakeTestData.length, 100);
     });
   });
 
-  describe('Seeding Script', function() {
+  describe('Seeding Script', () => {
 
     mongoose.connect('mongodb://localhost/fec_product_features');
     mongoose.connection
-      .once('open', () => console.log('Database connected for testing!'))
       .on('error', (error) => {
         console.warn('Error: ', error);
-      })
+      });
 
     beforeEach((done) => {
       db.collections.productfeatures.drop(() => {
         done();
-      })
-    })
+      });
+    });
 
-    it('creates and saves records in the database sucessfully', (done) => {
+    afterEach((done) => {
+      db.collections.productfeatures.drop(() => {
+        done();
+      });
+    });
+
+    it('creates and saves record(s) in the database sucessfully', (done) => {
       let fakeTestData = fakeDataGenerator(1, 0);
       let fakeProductFeatures = new ProductFeatures(fakeTestData);
       fakeProductFeatures.save()
@@ -39,13 +44,18 @@ describe('Database seeding', function() {
         })
         .finally(() => mongoose.connection.close());
     });
+
+    xit('throws an error if no parameters are passed into fakeDataGenerator', (done) => {
+      expect(fakeDataGenerator()).to.throw(new Error('Must include recordsToCreate and startingRecordID params'))
+      done();
+    })
   });
 });
 
 describe('Express Server', () => {
-  const url = 'http://localhost:4000/';
+  const url = 'http://localhost:4000/:id';
   describe('Loading Express', () => {
-    it('should return status 200 at "http://localhost:4000/"', (done) => {
+    it('should return status 200 at "http://localhost:4000/:id"', (done) => {
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(200);
         done();
