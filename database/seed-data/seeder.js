680b4e776493a7aa1c6e9a458d6e95cba12e3764
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-22 20:47:15 -0800

    Refactor to catch errors

--- database/seed-data/seeder.js
+++ database/seed-data/seeder.js
@@ -3,13 +3,19 @@ const ProductFeatures = require('../database.js');
 const { fakeDataGenerator } = require('./fakeDataGenerator.js');
 
 (async function seedDatabase() {
-  // load fakeDataGenerator with (recordsToCreate, startingRecordID)
-  let fakeData = await fakeDataGenerator(100, 1000);
+  try {
+    // load fakeDataGenerator with (recordsToCreate, startingRecordID)
+    let fakeData = await fakeDataGenerator(100, 1000);
+    console.log(`Seeding database with ${fakeData.length} record(s).`);
 
-  console.log(`Seeding database with ${fakeData.length} record(s).`);
-
-  ProductFeatures.insertMany(fakeData)
+    ProductFeatures.insertMany(fakeData)
     .then(() => console.log('Database seeding complete.'))
     .catch((err) => console.log('Error seeding database', err))
     .finally(() => mongoose.connection.close());
+  }
+  catch(error) {
+    // catch fakeDataGenerator error
+    console.log(error);
+    mongoose.connection.close();
+  }
 })();
