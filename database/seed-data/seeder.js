7b948a330fe6e0d68e6018fa0e6a4711a7e2e80d
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-24 15:40:35 -0800

    Fix: import mongoose connection into seeder script

--- database/seed-data/seeder.js
+++ database/seed-data/seeder.js
@@ -7,6 +7,16 @@ async function seedDatabase(recordsToCreate, startingRecordID) {
     let fakeData = await fakeDataGenerator(recordsToCreate, startingRecordID);
     console.log(`Seeding database with ${fakeData.length} record(s).`);
 
+    mongoose.connect('mongodb://localhost/fec_product_features', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connection.once('open', () => console.log('Mongoose connection established for seeding.'));
+
     ProductFeatures.insertMany(fakeData)
     .then(() => console.log('Database seeding complete.'))
     .catch((err) => console.log('Error seeding database', err))
