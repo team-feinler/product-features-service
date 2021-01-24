eabd3ccad11b8925a92a392f683dacd37e49689f
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-23 16:04:29 -0800

    Refactor database query to return error for query not found

--- database/database.js
+++ database/database.js
@@ -32,10 +32,19 @@ const ProductFeatures = mongoose.model('ProductFeatures', productFeaturesSchema)
 const load = (productId, callback) => {
   ProductFeatures.find({ productId: productId })
     .exec((err, data) => {
+      // if error, return error
       if (err) {
         console.log(`Error loading product ${productId} from database`, err);
         callback(err);
-      } else {
+      }
+      // else if product does not exist, create and send error
+      // db.collections.find() does not return error when no query match
+      else if (data[0] === undefined || !data[0].productId) {
+        console.log(`Error: product ${productId} does not exist`);
+        callback(new Error('Product not found!'));
+      }
+      // else document record at productId exists, so send data
+      else {
         callback(null, data);
       }
     });
