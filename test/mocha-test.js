44479432cf8c19a28075a5a8255a01b312dd707b
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-23 16:08:19 -0800

    Complete server and database unit tests

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,14 +1,11 @@
-const server = require('../server/server.js');
 const mongoose = require('mongoose');
-const db = mongoose.connection;
 const ProductFeatures = require('../database/database.js');
 const { fakeDataGenerator } = require('../database/seed-data/fakeDataGenerator.js');
 
-// require dev-dependencies
+// require dev-dependencies for testing
 const assert = require('assert');
-const chai = require('chai'), chaiHttp = require('chai-http');
+const chai = require('chai'), chaiHttp = require('chai-http'), expect = require('chai').expect;
 chai.use(chaiHttp);
-const expect = require('chai').expect;
 const request = require('request');
 
 
@@ -26,50 +23,67 @@ describe('Database seeding', () => {
       done();
     });
 
+    // open mongoose connection for database saving
     mongoose.connect('mongodb://localhost/fec_product_features');
-    mongoose.connection
-      .on('error', (error) => {
-        console.warn('Error: ', error);
-      });
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
+    // before running testing, delete test document record
     beforeEach((done) => {
-      db.collections.productfeatures.drop(() => {
+      ProductFeatures.deleteOne({ productId: 1 }, (err) => {
         done();
       });
     });
 
-    it('creates and saves record(s) in the database sucessfully', (done) => {
-      let fakeTestData = fakeDataGenerator(1, 0);
-      let fakeProductFeatures = new ProductFeatures(fakeTestData);
+    it('creates and saves record(s) in the database successfully', (done) => {
+      let fakeTestData = fakeDataGenerator(1, 1);
+      let fakeProductFeatures = new ProductFeatures(fakeTestData[0]);
       fakeProductFeatures.save()
         .then(() => {
           assert(!fakeProductFeatures.isNew);
-          done();
         })
-        .finally(() => mongoose.connection.close());
+        .finally(() => {
+          mongoose.connection.close();
+          done();
+        });
     });
   });
 });
 
 describe('Express Server', () => {
-  const url = 'http://localhost:4000/:id';
-  describe('Loading Express', () => {
+  describe('Express Middleware', () => {
     it('should return status 200 at "http://localhost:4000/:id"', (done) => {
-      request(url, (err, res, body) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+      chai.request(require('../server/server.js'))
+        .get('/:id')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          done();
+        });
     });
 
     it('should GET a single "product feature" record', (done) => {
-      chai.request(server)
-        .get('/product-features/1')
-        .end((err, res) => {
-          // expect(res.statusCode).to.equal(200)
-          console.log(res.body);
-          expect(res.body).to.be.a('object')
-          done();
+      chai.request(require('../server/server.js'))
+        .get('/product-features/:id')
+        .query({
+          productId: 1000
         })
-    })
+        .end((err, res) => {
+          expect(res.body).to.be.a('array')
+          expect(res.body[0].productId).to.equal(1000)
+          done();
+        });
+    });
+
+    it('should return 404 for "product feature" record that does not exist', (done) => {
+      chai.request(require('../server/server.js'))
+        .get('/product-features/:id')
+        .query({
+          productId: 9000
+        })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(404);
+          mongoose.connection.close();
+          done();
+        });
+    });
   });
 });
