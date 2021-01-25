6bf6e5de68b98f4bea479b9964c22697780aa340
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-24 17:31:49 -0800

    Update webpack for css

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   }
 }
