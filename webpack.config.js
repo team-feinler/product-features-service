d48bb5fd04d1a80eb2b91dac0d40133556019636
jrozal <68182930+jrozal@users.noreply.github.com>
2021-01-30 18:24:49 -0800

    Update webpack config for styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         include : SRC_DIR,
         loader : 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react', ["babel-plugin-styled-components"]]
         }
       },
       {
