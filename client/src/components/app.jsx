341f58c96bf45dd5b1b033c46c204fd8075cee74
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-06 18:44:58 -0800

    Update gitignore

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -6,7 +6,8 @@ import Banner from './banner.jsx';
 import Features from './features.jsx';
 import Setup from './setup.jsx';
 import AdditionalFeatures from './additionalFeatures.jsx';
-import GlobalStyles from '../globalStyles.js'
+import GlobalStyles from '../globalStyles.js';
+// import Spinner from './spinner.gif';
 import dummyImages from '../dummyImages.js';
 
 class ProductFeatures extends Component {
@@ -14,14 +15,14 @@ class ProductFeatures extends Component {
     super(props);
     this.state = {
       productFeatures: [],
-      productPhotos: dummyImages // pending call to photos service in componentDidMount()
+      productPhotos: dummyImages,
+      isLoading: true
     }
   }
 
   getProductFeatures(productId) {
     axios.get(`http://localhost:4000/product-features/${productId}`)
       .then((response) => {
-        console.log(response.data);
         this.setState({
           productFeatures: response.data
         });
@@ -32,10 +33,10 @@ class ProductFeatures extends Component {
   }
 
   getProductPhotos(productId) {
-    axios.get(`http://localhost:4002/photos/features/${productId}`)
+    axios.get(`http://localhost:4002/photos/id/${productId}`)
     .then((response) => {
       this.setState({
-        productPhotos: response.data.featuresUrls
+        productPhotos: response.data.productUrls
       });
     })
     .catch((error) => {
@@ -43,36 +44,39 @@ class ProductFeatures extends Component {
     });
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const productId = window.location.pathname.split('/')[1] || 1000;
-    console.log('productId:', productId);
-    this.getProductFeatures(productId);
-    this.getProductPhotos(productId); // pending service availability
+    await this.getProductFeatures(productId);
+    // this.getProductPhotos(productId); // pending service availability
+    this.setState({isLoading: false});
   }
 
   render() {
     const text = this.state.productFeatures[0];
     const img = this.state.productPhotos;
     return (
-      (text === undefined
-        ? <h1 style={{textAlign: 'center'}}>Product Features Service Unavailable 404</h1>
-        : <div>
-            <GlobalStyles />
-            <Banner
-              bannerText={text.banner}
-              bannerImage={img} />
-            <Features
-              featuresText={text.features}
-              featuresPhotos={img} />
-            <Setup
-              featuresText={text.featureSetup}
-              featuresSetupPhotos={img} />
-            <AdditionalFeatures
-              additionalFeaturesText={text.additionalFeatures}
-              additionalFeaturesPhotos={img} />
-          </div>
+      (this.state.isLoading
+        ? <div>Loading...</div>
+        : (text === undefined
+            ? <h1 style={{textAlign: 'center'}}>Product Features Service Unavailable 404</h1>
+            : <div>
+                <GlobalStyles />
+                <Banner
+                  bannerText={text.banner}
+                  bannerImage={img} />
+                <Features
+                  featuresText={text.features}
+                  featuresPhotos={img} />
+                <Setup
+                  featuresText={text.featureSetup}
+                  featuresSetupPhotos={img} />
+                <AdditionalFeatures
+                  additionalFeaturesText={text.additionalFeatures}
+                  additionalFeaturesPhotos={img} />
+              </div>
+          )
       )
-    );
+    )
   }
 }
 
