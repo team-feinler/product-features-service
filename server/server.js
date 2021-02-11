251310aa9aa4b2a77450e266036206074c332965
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-10 23:35:08 -0800

    Update server file

--- server/server.js
+++ server/server.js
@@ -1,37 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const db = require('../database/database.js');
 
 app.use(cors());
-
-// keeping for development
-// app.use('/', express.static(__dirname + '/../client/dist'));
-// app.use('/:id', express.static(__dirname + '/../client/dist'));
-
-// use for proxy server
-app.use('/', express.static(__dirname + '/../public'));
-app.use('/:id', express.static(__dirname + '/../public'));
+app.use('/', express.static(path.join(__dirname + '/../public')));
+app.use('/:id', express.static(path.join(__dirname + '/../public')));
 
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200
 }
 
-
-// open mongoose connection
-mongoose.connect('mongodb://localhost/fec_product_features', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
-
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-
-// express middleware
 app.get('/product-features/:id', (req, res) => {
   const productId = req.params.id;
   console.log(`Requesting product ${productId} from the database.`)
