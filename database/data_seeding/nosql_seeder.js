bed5ee6ac5ad8caad42cfa6116a2985d0e6f670e
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-03-10 20:19:53 -0800

    Complete seeding script for nosql DB

--- database/data_seeding/nosql_seeder.js
+++ database/data_seeding/nosql_seeder.js
@@ -2,45 +2,44 @@ const { user, password } = require('../../database_configs/nosql_database.config
 const nano = require('nano')(`http://${user}:${password}@localhost:5984`);
 const { generateData } = require('./data_generator.js');
 
-const destroyDb = async (dbName) => {
-  await nano.db.destroy(dbName);
-}
-
-const createDb = async (dbName) => {
-  await nano.db.create(dbName)
-    .then((res) => {
-      console.log('Database successfully created', res);
-    })
-    .catch((err) => {
-      console.log('Error creating database', err);
-    });
-}
-
-const insertData = (data) => {
-  // code
-}
-
 const seedNoSqlData = async (numRecords, pace) => {
 
-  await destroyDb('product_features');
-  await createDb('product_features');
+  if ((numRecords / pace) - Math.floor(numRecords / pace) !== 0) {
+    throw new Error('Number of records must be divisible by pace (ex: 1,000 records at a pace of 100)');
+  }
+
+  const numBatches = numRecords / pace;
+  const copyOfNumRecords = numRecords;
+
+  try {
+    await nano.db.destroy('product_features');
+    await nano.db.create('product_features');
+  } catch (err) {
+    throw new Error('Error initializing database: ', err);
+  }
+
   const pf = nano.use('product_features');
 
-  // while (numRecords < 0) {
-  //   // seed
-  // }
-  const data = generateData(5, 1, 'nosql');
+  let currentBatch = 1;
+  let startingId = 1;
 
-  data.map((record) => {
-    const { productIdEncid } = record;
-    pf.insert(record, productIdEncid)
-      .then((res) => {
-        console.log('Inserted record successfully', res);
-      })
-      .catch((err) => {
-        console.log('Error inserting record', err);
-      });
-  });
+  while (numRecords > 0) {
+    const data = generateData(1000, startingId, 'nosql');
+
+    await pf.bulk({ docs: data })
+    .then((res) => {
+      console.log(`Batch number ${currentBatch} / ${numBatches} successfully seeded for IDs ${startingId} through ${(startingId + pace) - 1}`);
+    })
+    .catch((err) => {
+      throw new Error(`Error seeding database on batch number ${currentBatch} / ${numBatches} for IDs ${startingId} through ${(startingId + pace) - 1}`);
+    });
+
+    numRecords -= pace;
+    startingId += pace;
+    currentBatch++;
+  }
+
+  console.log(`Database seeding for ${copyOfNumRecords} records complete`);
 }
 
 seedNoSqlData(10000000, 1000);
