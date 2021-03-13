3a153f7b1ef200901956acd17350c1e67676f0ff
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-03-13 15:01:58 -0800

    Complete schema design for SQL DB

--- schema.sql
+++ schema.sql
@@ -4,9 +4,31 @@ CREATE DATABASE product_features;
 
 \c product_features;
 
-CREATE TABLE fake_table (
-  id uuid PRIMARY KEY,
-  firstName VARCHAR(200) NOT NULL,
-  middleName VARCHAR(200) DEFAULT NULL,
-  lastName VARCHAR(200) DEFAULT NULL
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE features (
+  id_encid uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  id_decid SERIAL NOT NULL UNIQUE,
+  feature_banner_header TEXT NOT NULL,
+  feature_banner_text TEXT NOT NULL,
+  feature_setup_header TEXT NOT NULL,
+  feature_setup_description TEXT NOT NULL,
+  additional_features_header TEXT NOT NULL,
+  additional_features_description TEXT NOT NULL
+);
+
+CREATE TABLE features_list (
+  id_encid uuid DEFAULT uuid_generate_v4() PRIMARY KEY ,
+  id_decid SERIAL NOT NULL,
+  header TEXT NOT NULL,
+  description TEXT NOT NULL,
+  feature_id_decid INT NOT NULL REFERENCES features (id_decid)
+);
+
+CREATE TABLE content_grid_feature_items (
+  id_encid uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  id_decid SERIAL NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  feature_id_decid INT NOT NULL REFERENCES features (id_decid)
 );
